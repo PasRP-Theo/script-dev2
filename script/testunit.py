db5ab9e70c8481f69ebf3f4d0f9f3cc425724a77
PasRP-Theo <theomertens12345@gmail.com>
2024-12-27 10:22:14 +0100

    Update testunit.py

--- script/testunit.py
+++ script/testunit.py
@@ -1,6 +1,6 @@
 import unittest
 import pandas as pd
-from script import load_csv_files, search_product, filter_by_category, filter_by_price, generate_report
+from script import chargement_csv, chercher_produit, filtrer_par_cat, filtrer_par_prix, generer_rapport
 
 class TestInventoryManager(unittest.TestCase):
 
@@ -13,26 +13,26 @@ class TestInventoryManager(unittest.TestCase):
         })
 
     def test_load_csv_files(self):
-        df = load_csv_files('path_to_your_test_data')
+        df = chargement_csv('path_to_your_test_data')
         self.assertFalse(df.empty)
 
     def test_search_product(self):
-        results = search_product(self.data, 'riz')
+        results = chercher_produit(self.data, 'riz')
         if results is not None:
             self.assertEqual(len(results), 1)
         else:
             self.fail("search_product returned None")
 
     def test_filter_by_category(self):
-        results = filter_by_category(self.data, 'alimentaire')
+        results = filtrer_par_cat(self.data, 'alimentaire')
         self.assertEqual(len(results), 2)
 
     def test_filter_by_price(self):
-        results = filter_by_price(self.data, min_price=1.0, max_price=2.0)
+        results = filtrer_par_prix(self.data, min_price=1.0, max_price=2.0)
         self.assertEqual(len(results), 2)
 
     def test_generate_report(self):
-        summary = generate_report(self.data)
+        summary = generer_rapport(self.data)
         self.assertIn('Total Quantité', summary.columns)
         self.assertIn('Prix Moyen', summary.columns)
 
